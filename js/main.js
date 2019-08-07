0822ce7f7729087cc5a441d028a41f2772857ead
Alexander <mrreads@yandex.com>
2019-08-07 03:27:17 +0300

    scroll hide

--- js/main.js
+++ js/main.js
@@ -3,12 +3,7 @@ function hideContant()
     document.querySelector(".contact").style.opacity = "0";
     document.querySelector(".contact").style.zIndex = "-1";
     document.querySelector(".block:nth-child(5)").style.justifyContent = "space-evenly";
-    
     document.querySelector(".block:nth-child(5)").style.Width = document.querySelector(".block:nth-child(5)").offsetWidth + "px";
-    
-    //document.querySelector(".block:nth-child(5)").style.maxWidth = document.querySelector(".block:nth-child(5)").offsetWidth + "px";
-    //document.querySelector(".block:nth-child(5)").style.minWidth = document.querySelector(".block:nth-child(5)").offsetWidth + "px";
-    
     document.querySelector(".vk").style.display = "flex";
     document.querySelector(".github").style.display = "flex";
     document.querySelector(".codepen").style.display = "flex";
