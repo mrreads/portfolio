722aa3e3b40336c2c8b94237e79f890f7aa77390
Alexander <mrreads@yandex.com>
2020-12-14 16:02:27 +0300

    remove preloader | adedd scrollreveal

--- js/init.js
+++ js/init.js
@@ -1,8 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-    setTimeout(() => {
-        document.querySelector('.preloader').style.opacity = '0';
-        setTimeout(() => {
-            document.querySelector('.preloader').remove();
-        }, 500);
-    }, 100);
+    ScrollReveal().reveal('.headline', { reset: true });
 });
