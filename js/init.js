dde04ed4797c2d57cca4fbbc0b5c7c0c74f2affa
Alexander <mrreads@yandex.com>
2020-12-19 20:46:59 +0300

    full reword | added  type plugin

--- js/init.js
+++ js/init.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-    ScrollReveal().reveal('.anim', { reset: true });
-});
+ScrollReveal().reveal('.anim', { reset: true });
+
+new TypeIt("#type", {
+    speed: 50,
+    startDelay: 900
+  })
+.type("Я - разработчик сайтов", {delay: 100})
+.delete(7)
+.move(-10, {speed: 50, delay: 100})
+.delete(5)
+.type("Веб-р", {delay: 200})
+.move(10, {speed: 150, delay: 100})
+.type(".", {delay: 250})
+.delete(1)
+.type(", дизайнер.", {delay: 2250})
+.go();
